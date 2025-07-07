53dbeb6dafea8e076883bdf53110f5b34c0859bb
Tahim Bhuiya <tbhuiya2018@fau.edu>
2025-07-07 17:42:48 -0400

    Added code to actually calculate averages

--- FCFS (Non-Preemptive) Scheduler.py
+++ FCFS (Non-Preemptive) Scheduler.py
@@ -115,6 +115,10 @@ total_turnaround_time = sum(p.turnaround_time for p in completed_processes)
 total_response_time = sum(p.response_time for p in completed_processes)
 num_processes = len(completed_processes)
 
+average_waiting_time = total_waiting_time / num_processes
+average_turnaround_time = total_turnaround_time / num_processes
+average_response_time = total_response_time / num_processes
+
 # Display final results
 print("\nResults at the end of the simulation:")
 print(f"Total time needed to complete all processes: {total_time}")
